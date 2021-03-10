bc0b6ebc2f30853ef614b1e40734dee363476068
raghib1992 <65724939+raghib1992@users.noreply.github.com>
2021-03-10 18:29:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "aws_key_pair" "mykey" {
 }
 
 resource "aws_instance" "example" {
-    ami = var.AMIS[var.AWS_REGION]
+    ami = var.AMIS[var.REGION]
     instance_type = "t2.micro"
     key_name = aws_key_pair.mykey.key_name
 # iam_instance_profile = aws_iam_instance_profile.s3-access-instance-profile.name  
